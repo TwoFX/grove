606b91f03d17288cb6c0bae944a3d99a6db92b43
Markus Himmel <markus@lean-fro.org>
2025-06-18 17:43:13 +0200

    Get from overview to table

--- frontend/src/widgets/association-table/AssociationTableComponent.tsx
+++ frontend/src/widgets/association-table/AssociationTableComponent.tsx
@@ -6,5 +6,16 @@ export function AssociationTableComponent({
 }: {
   associationTable: AssociationTable;
 }): JSX.Element {
-  return <p>{associationTable.definition.widgetId}</p>;
+  return (
+    <p>
+      Hello, there is an association table with the id{" "}
+      {associationTable.definition.widgetId},{" "}
+      <a
+        href={`/association/${associationTable.definition.widgetId}`}
+        className="px-3 py-1 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
+        yes please take me there.
+      </a>
+    </p>
+  );
 }
