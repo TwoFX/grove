82dab008314ec9abdc2249bda476f4588cb3ea89
Markus Himmel <markus@lean-fro.org>
2025-07-28 14:26:58 +0200

    Close #38

--- frontend/src/components/HashCheck.tsx
+++ frontend/src/components/HashCheck.tsx
@@ -1,4 +1,4 @@
-import { useCountPendingChanges } from "@/lib/state/pending";
+import { useCountPendingChanges, usePendingChanges } from "@/lib/state/pending";
 import { useGroveAdminStore } from "@/lib/state/state";
 import { GroveContext } from "@/lib/transfer/context";
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
@@ -9,6 +9,7 @@ function HashConflictDialog(): JSX.Element {
   const basisHash = useContext(GroveContext).projectMetadata.hash;
   const storedHash = useGroveAdminStore((state) => state.hash);
   const setStoredHash = useGroveAdminStore((state) => state.setHash);
+  const pendingChanges = usePendingChanges();
   const pendingCount = useCountPendingChanges();
   const hasHydrated = useGroveAdminStore((state) => state.hasHydrated);
 
@@ -28,18 +29,28 @@ function HashConflictDialog(): JSX.Element {
             State conflict
           </DialogTitle>
           <p>
-            You have unsaved work based on library hash <b>{storedHash}</b>, but
+            You have pending work based on library hash <b>{storedHash}</b>, but
             the copy of Grove you have opened was generated from the library
-            hash <b>{basisHash}</b>. You have several options:
+            hash <b>{basisHash}</b>.
           </p>
+          <p>Specifically, there are the following pending changes:</p>
+          <ul>
+            {pendingChanges.map((change) => (
+              <li key={change.id}>{change.displayShort}</li>
+            ))}
+          </ul>
+          <p>You have several options:</p>
           <ul className="list-disc list-inside space-y-1 ml-4">
             <li>
               Close this copy of Grove now and open one that was built from{" "}
               <b>{storedHash}</b>, then save your work.
             </li>
-            <li>Clear (= lose!) all unsaved data.</li>
             <li>
-              Work with the unsaved data on top of the new library, possibly
+              Clear (= lose!) all unsaved data. This is the right thing to do if
+              you have previously saved the pending changes.
+            </li>
+            <li>
+              Work with the pending data on top of the new library, possibly
               introducing inconsistencies.
             </li>
           </ul>
