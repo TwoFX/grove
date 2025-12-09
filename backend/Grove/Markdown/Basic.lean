c0eaefcbf5dee25baa52f9daf135ea397c52426b
Markus Himmel <markus@lean-fro.org>
2025-12-09 14:59:57 +0100

    Add headers to Markdown

--- backend/Grove/Markdown/Basic.lean
+++ backend/Grove/Markdown/Basic.lean
@@ -7,6 +7,9 @@ module
 namespace Grove.Markdown
 
 public inductive Paragraph where
+  | h1 : String → Paragraph
+  | h2 : String → Paragraph
+  | h3 : String → Paragraph
   | text : String → Paragraph
   | codeBlock : String → Paragraph
   | blockQuote : String → Paragraph
@@ -15,6 +18,9 @@ public instance : Coe String Paragraph where
   coe := .text
 
 def Paragraph.render : Paragraph → String
+  | h1 t => s!"# {t}"
+  | h2 t => s!"## {t}"
+  | h3 t => s!"### {t}"
   | text t => t
   | codeBlock c => s!"```\n{c}{if c.endsWith "\n" then "" else "\n"}```"
   | blockQuote b => String.intercalate "\n" <| List.map ("> " ++ ·) <| b.splitOn "\n"
